95538a6da325b89c450286a09669b34eb3ad8d84
Simon Meoni <simonmeoni@aol.com>
2021-01-14 15:33:01 +0100

    I used cookiecutter to have a project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a machine learning project for kaggle tweet sentiment extraction competition',
+    author='Simon Meoni',
+    license='MIT',
+)
