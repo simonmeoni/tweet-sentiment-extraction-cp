2d05f0ed4dbc54b043e9eaa0cb475644aba248ac
Simon Meoni <simonmeoni@aol.com>
2021-01-17 02:37:16 +0100

    * just extract span

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
+import logging
+import re
 from concurrent import futures
+from pathlib import Path
 
 import click
-import logging
 import pandas as pd
-import re
-from tokenizers import Tokenizer, models, pre_tokenizers, decoders, processors
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
-
-from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from tokenizers import Tokenizer, models
+from tokenizers.trainers import BpeTrainer
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-@click.argument('model', default="from_scratch")
+@click.argument('model', default="custom_model")
 def main(input_filepath, output_filepath, model):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -28,8 +26,7 @@ def main(input_filepath, output_filepath, model):
     train = lowercase_and_replace_url(train, 'text')
     train = lowercase_and_replace_url(train, 'selected_text')
     test = lowercase_and_replace_url(test, 'text')
-    tokenizer = None
-    if model == 'from_scratch':
+    if model == 'custom_model':
         write_learning_dataset(output_filepath + '/tokenizer_dataset.txt',
                                pd.concat([train, test], ignore_index=True))
 
@@ -39,53 +36,29 @@ def main(input_filepath, output_filepath, model):
                              show_progress=True)
         tokenizer.train([output_filepath + '/tokenizer_dataset.txt'], trainer=trainer)
         tokenizer.save(output_filepath + "/from_scratch-tokenizer.json")
+    train = extract_span(train)
+    train.to_pickle(output_filepath + '/train_processed_{}.pickle'.format(model))
+    test.to_pickle(output_filepath + '/test_processed_{}.pickle'.format(model))
 
-    train = extract_and_encode_sel_vector(train, tokenizer)
-    train.to_pickle(output_filepath + '/train_processed.pickle')
-    test.to_pickle(output_filepath + '/test_processed.pickle')
 
-
-def extract_and_encode_sel_vector(dataframe, tokenizer):
-    dataframe["text_vector"] = ''
-    dataframe["selected_text_vector"] = ''
-    dataframe["inter_vector"] = ''
+def extract_span(dataframe):
     dataframe["span"] = ""
     with futures.ThreadPoolExecutor(max_workers=1) as executor:
         future_text = {
-            executor.submit(extract_and_encode_sel_vector_thread, df_entry, df_idx, tokenizer):
+            executor.submit(extract_span_thread, df_entry, df_idx):
                 df_entry for df_idx, df_entry in enumerate(dataframe.iloc)}
         for future in futures.as_completed(future_text):
             res = future.result()
-            dataframe.at[res[0], 'text_vector'] = res[1]
-            dataframe.at[res[0], 'selected_text_vector'] = res[2]
-            dataframe.at[res[0], 'inter_vector'] = res[3]
-            dataframe.at[res[0], 'span'] = res[-1]
+            dataframe.at[res[0], 'span'] = res[1]
 
-    dataframe = dataframe[dataframe['selected_vector'] != []]
     dataframe = dataframe.reset_index(drop=True)
     return dataframe
 
 
-def extract_and_encode_sel_vector_thread(df_entry, df_idx, tokenizer):
-    text_sub_tokens = tokenizer.encode(df_entry['text'])
-    selected_text_sub_tokens = []
-    inter_vector = []
+def extract_span_thread(df_entry, df_idx):
     start_offset = df_entry['text'].find(df_entry['selected_text'])
-    start_index = -1
-    end_index = -1
-    if start_offset != -1:
-        inter_vector = [0] * len(text_sub_tokens)
-        end_offset = start_offset + len(df_entry['selected_text'])
-        for idx_offset, offset in enumerate(text_sub_tokens.offsets):
-            if offset[0] <= start_offset <= offset[1]:
-                start_index = idx_offset
-            if offset[0] <= end_offset <= offset[1]:
-                end_index = idx_offset
-                break
-        selected_text_sub_tokens = text_sub_tokens.ids[start_index:end_index + 1]
-        inter_vector[start_index:len(selected_text_sub_tokens)] = [1] * len(selected_text_sub_tokens)
-    return df_idx, text_sub_tokens.ids, selected_text_sub_tokens, inter_vector, \
-           [start_index, end_index]
+    end_offset = start_offset + len(df_entry['selected_text'])
+    return df_idx, [start_offset, end_offset]
 
 
 def write_learning_dataset(path, dataframe):
