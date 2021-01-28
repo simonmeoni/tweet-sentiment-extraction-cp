fc575972413507246af19b9894574ed8d9b81948
Simon Meoni <simonmeoni@aol.com>
2021-01-28 14:47:55 +0100

    * add new dataset class for seq classification task * add new file to train a model for seq classification task * change make dataset to replace strange quote by "regular" quote

---
+++ src/data/token_classification_tse_dataset.py
@@ -0,0 +1,88 @@
+import torch
+import pandas as pd
+from tqdm import tqdm
+from torch.utils.data import Dataset
+
+
+class TokenClassificationTweetDataset(Dataset):
+    def __init__(self, dataset, tokenizer):
+        self.tokenizer = tokenizer
+        self.dataset = self.init_dataset(dataset)
+        self.pad_id = tokenizer.pad_token_id
+
+    def __getitem__(self, index):
+        entry = self.dataset.loc[index]
+        return {
+            'x_obs': entry['tokens'],
+            'attention_mask': [],
+            'target': entry['selected_tokens'],
+            'text': entry['text'],
+            'selected_text': entry['selected_text']
+        }
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def token_classification_collate(self, batch):
+        max_len = max(len(el['x_obs']) for el in batch)
+        res_batch = {'x_obs': [],
+                     'attention_mask': [],
+                     'target': [],
+                     'selected_text': [],
+                     'text': [],
+                     }
+        for input_batch in batch:
+            vector = input_batch['x_obs']
+            padded_list = [self.pad_id] * (max_len - len(vector))
+            x_obs = input_batch['x_obs'] + padded_list
+            res_batch['x_obs'].append(torch.LongTensor(x_obs))
+            res_batch['target'].append(torch.LongTensor(input_batch['target'] + padded_list))
+            res_batch['attention_mask'].append(torch.LongTensor([1 if token != self.pad_id else 0
+                                                                 for token in x_obs]))
+            res_batch['text'].append(input_batch['text'])
+            res_batch['selected_text'].append(input_batch['selected_text'])
+        res_batch = {k: (torch.stack(v) if isinstance(v[0], torch.Tensor) else v)
+                     for k, v in res_batch.items()}
+        return res_batch
+
+    def init_dataset(self, dataset):
+        print('Initialize Dataset ....')
+        new_dataset = pd.concat([dataset,
+                                 pd.DataFrame(columns=["selected_tokens", "tokens"])])
+        for df_idx, df_entry in tqdm(new_dataset.iterrows(),
+                                     total=new_dataset.shape[0],
+                                     desc="span token extractions: "):
+            res = self.extract_span_tokens(df_entry, df_idx)
+            new_dataset.at[res[0], "tokens"] = res[1]
+            new_dataset.at[res[0], "selected_tokens"] = res[2]
+
+        new_dataset = new_dataset.reset_index(drop=True)
+
+        return new_dataset
+
+    def extract_span_tokens(self, df_entry, df_idx):
+        text = df_entry["text"]
+        tokens = self.tokenizer(text, return_offsets_mapping=True)
+        ids = tokens['input_ids'][1:-1]
+        selected_tokens = [0] * len(ids)
+        end_idx, start_idx = self.get_span_token_index(
+            df_entry["span"][1],
+            tokens['offset_mapping'][1:-1],
+            df_entry["span"][0])
+        selected_tokens[start_idx:end_idx + 1] = [0] + [1] * \
+                                                 len(selected_tokens[start_idx:end_idx + 1]) + [0]
+        return df_idx, tokens['input_ids'], selected_tokens
+
+    @staticmethod
+    def get_span_token_index(end, offsets, start):
+        start_idx = 0
+        end_idx = 0
+        for idx_offset, offset in enumerate(offsets):
+            start_token = offset[0]
+            end_token = offset[1]
+            if start_token <= start <= end_token:
+                start_idx = idx_offset
+            if start_token <= end <= end_token:
+                end_idx = idx_offset
+                break
+        return end_idx, start_idx
---
+++ src/models/bert_token_classification_train.py
@@ -0,0 +1,196 @@
+import logging
+import os
+from datetime import datetime
+from pathlib import Path
+
+import click
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import torch.optim as optim
+from numpy import mean
+from dotenv import find_dotenv, load_dotenv
+from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader
+from tokenizers import Tokenizer
+from transformers import DistilBertForMaskedLM, DistilBertConfig
+from src.data.masked_lm_tse_dataset import MaskedLMTweetDataset
+import wandb
+
+# pylint: disable=too-many-arguments, too-many-locals
+
+
+def init_model(tokenizer, config, device):
+    print("🍌 Loading model...")
+    max_length = config["max_length"]
+    model_parameter_path = DistilBertConfig(vocab_size=tokenizer.get_vocab_size(),
+                                            max_position_embeddings=max_length,
+                                            num_attn_heads=config["num_attn_heads"],
+                                            n_layers=config["n_layers"],
+                                            hidden_dim=config["hidden_dim"],
+                                            type_vocab_size=1,
+                                            dropout=0.1,
+                                            attention_dropout=0.1,
+                                            pad_token_id=0)
+    model = DistilBertForMaskedLM(config=model_parameter_path)
+    model.to(device)
+    print(f'The model has {count_parameters(model):,} trainable parameters')
+    return model
+
+
+def init_tokenizer(tokenizer_path):
+    print("🍌 Loading tokenizer...")
+    tokenizer = Tokenizer.from_file(tokenizer_path)
+    return tokenizer
+
+
+def count_parameters(mdl):
+    return sum(p.numel() for p in mdl.parameters() if p.requires_grad)
+
+
+def compute_loss(predictions, targets, criterion=None):
+    predictions = predictions[:, :-1, :].contiguous()
+    targets = targets[:, 1:]
+    rearranged_output = predictions.view(predictions.shape[0] * predictions.shape[1], -1)
+    rearranged_target = targets.contiguous().view(-1)
+    loss = criterion(rearranged_output, rearranged_target)
+
+    return loss
+
+
+def train_model(model, dataloader, optimizer, criterion, device, id_fold):
+    model.train()
+    epoch_loss = []
+    for batch in dataloader:
+        optimizer.zero_grad()
+        out = model(input_ids=batch['inputs_ids'].to(device),
+                    attention_mask=batch['attention_mask'].to(device))
+        prediction_scores = out.logits
+        predictions = f.log_softmax(prediction_scores, dim=2)
+        loss = compute_loss(predictions, batch['inputs_ids'], criterion)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+        wandb.log({"loss {}".format(id_fold): loss.item()})
+        epoch_loss.append(loss.item())
+    print('train epoch loss : {}'.format(mean(epoch_loss)))
+
+
+def eval_model(model, dataloader, optimizer, criterion, device, id_fold):
+    model.eval()
+    epoch_loss = []
+    for batch in dataloader:
+        optimizer.zero_grad()
+        out = model(input_ids=batch['inputs_ids'].to(device),
+                    attention_mask=batch['attention_mask'].to(device))
+        prediction_scores = out.logits
+        predictions = f.log_softmax(prediction_scores, dim=2)
+        loss = compute_loss(predictions, batch['inputs_ids'], criterion)
+        epoch_loss.append(loss.item())
+        wandb.log({"CV loss {}".format(id_fold): mean(loss.item())})
+        break
+    return mean(epoch_loss)
+
+
+@click.command()
+@click.option('--learning_rate')
+@click.option('--batch_size')
+@click.option('--max_length')
+@click.option('--num_attn_heads')
+@click.option('--n_layers')
+@click.option('--hidden_dim')
+@click.option('--num_epochs')
+@click.option('--folds')
+@click.option('--model_name')
+@click.option('--model_path')
+@click.option('--tokenizer_path')
+@click.option('--dataset_path')
+def main(learning_rate,
+         batch_size,
+         max_length,
+         num_attn_heads,
+         n_layers,
+         hidden_dim,
+         num_epochs,
+         folds,
+         model_name,
+         model_path,
+         tokenizer_path,
+         dataset_path):
+    hyperparameter_defaults = dict(
+        lr=float(learning_rate),
+        batch_size=int(batch_size),
+        max_length=int(max_length),
+        num_attn_heads=int(num_attn_heads),
+        n_layers=int(n_layers),
+        hidden_dim=int(hidden_dim),
+        num_epochs=int(num_epochs),
+        folds=int(folds),
+        model_name=model_name,
+        model_path=model_path,
+        tokenizer_path=tokenizer_path,
+        dataset_path=dataset_path,
+    )
+    wandb.init(project="tweet-se-competition", config=hyperparameter_defaults)
+
+    txt_dataset = []
+    with open(hyperparameter_defaults['dataset_path']) as file:
+        for line in file:
+            txt_dataset.append(line[:-1])
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Using device:", device)
+    tokenizer = init_tokenizer(hyperparameter_defaults['tokenizer_path'])
+    criterion = nn.NLLLoss(ignore_index=tokenizer.token_to_id('[PAD]'))
+    folds = KFold(n_splits=hyperparameter_defaults['folds'], shuffle=False)
+    cv_score = []
+    for id_fold, fold in enumerate(folds.split(txt_dataset)):
+        print('beginning fold n°{}'.format(id_fold + 1))
+        model = init_model(tokenizer, hyperparameter_defaults, device)
+        optimizer = optim.Adam(model.parameters(), lr=hyperparameter_defaults['lr'])
+
+        train_fold, eval_fold = fold
+        train_fold = [txt_dataset[idx_train] for idx_train in train_fold]
+        eval_fold = [txt_dataset[idx_eval] for idx_eval in eval_fold]
+        train_dataset = MaskedLMTweetDataset(train_fold, tokenizer)
+        eval_dataset = MaskedLMTweetDataset(eval_fold, tokenizer)
+
+        train_dataloader = DataLoader(dataset=train_dataset,
+                                      batch_size=hyperparameter_defaults["batch_size"],
+                                      shuffle=False, drop_last=True,
+                                      collate_fn=train_dataset.masked_lm_collate)
+        eval_dataloader = DataLoader(dataset=eval_dataset,
+                                     batch_size=hyperparameter_defaults["batch_size"],
+                                     shuffle=False, drop_last=True,
+                                     collate_fn=eval_dataset.masked_lm_collate)
+
+        wandb.watch(model)
+        for epoch in range(hyperparameter_defaults['num_epochs']):
+            print("Starting epoch", epoch + 1)
+            train_model(model, train_dataloader, optimizer, criterion, device, id_fold)
+        print("Saving model ..")
+        current_datetime = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        save_location = hyperparameter_defaults['model_path']
+        model_name = hyperparameter_defaults['model_name'] + '-' + current_datetime + \
+                     '-fold-{}'.format(id_fold + 1)
+        if not os.path.exists(save_location):
+            os.makedirs(save_location)
+        save_location = os.path.join(save_location, model_name)
+        torch.save(model, save_location)
+        wandb.save(save_location)
+        score = eval_model(model, eval_dataloader, optimizer, criterion, device, id_fold)
+        cv_score.append(score)
+        wandb.log({"cv-score": score})
+    print('CV score : {}'.format(cv_score))
+
+
+if __name__ == '__main__':
+    LOG_FMT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=LOG_FMT)
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+    # pylint: disable=no-value-for-parameter
+    main()
