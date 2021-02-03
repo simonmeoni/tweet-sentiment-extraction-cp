74e9f10abbc0e83c20775750726804e34bd43ec1
Simon Meoni <simonmeoni@aol.com>
2021-02-03 10:10:56 +0100

    * add bumpversion to increase the version of the package for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='tweet-se-competition',
     packages=find_packages(),
-    version='0.1.5',
+    version='0.3.0',
     description='a machine learning project for kaggle tweet sentiment extraction competition',
     author='Simon Meoni',
     license='MIT',
